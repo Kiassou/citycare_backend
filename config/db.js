5fc110d8c5412f97326272979d9680fc5f1083d3
Kiassou <gaoussouthiero04@gmail.com>
2026-03-21 18:00:24 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,18 +2,12 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT) || 15825,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  // --- CONFIGURATION SSL OBLIGATOIRE POUR AIVEN ---
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: "mysql-23d58072-citycare.c.aivencloud.com",
+  user: "avnadmin",
+  password: "AVNS_tEEbVJZ7TmkBkDuTEHC",
+  database: "defaultdb",
+  port: 15825,
+  ssl: { rejectUnauthorized: false }
 });
 
 const db = pool.promise();
