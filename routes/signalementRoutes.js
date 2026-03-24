eaf71ce0b5fe7077a7a9cfe17e92ba43b7e3ac0c
Kiassou <gaoussouthiero04@gmail.com>
2026-03-24 01:39:08 +0000

    Update signalementRoutes.js

--- routes/signalementRoutes.js
+++ routes/signalementRoutes.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const path = require("path");
 const signalementController = require("../controllers/signalementController");
 
-// Stockage temporaire simple
-const upload = multer({ dest: "uploads/signalements/" });
+// Configuration du stockage des images
+const storage = multer.diskStorage({
+  destination: "uploads/signalements",
+  filename: (req, file, cb) => {
+    cb(null, `img_${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
 
-// --- Routes ---
+const upload = multer({ storage: storage });
 
-// Création : le champ doit s'appeler "photo"
+// Route POST avec le middleware 'upload.single'
 router.post("/", upload.single("photo"), signalementController.createSignalement);
 
-// Récupérer les signalements d'un utilisateur spécifique
-router.get("/user/:userId", signalementController.getUserSignalements);
+router.get("/user/:userId", signalementController.getUserSignalements,);
 
-// Supprimer un signalement
 router.delete("/:id", signalementController.deleteSignalement);
 
-// Récupérer tous les signalements (pour l'admin)
 router.get("/", signalementController.getAllSignalements);
 
-// Valider/Voter pour un signalement
-router.post("/validate", signalementController.validateSignalement);
+router.post("/validate", signalementController.validateSignalement,);
 
 module.exports = router;
