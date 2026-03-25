42568473986a83812c1a027399fadbd65cd2e727
Kiassou <gaoussouthiero04@gmail.com>
2026-03-25 19:21:19 +0000

    Update signalementRoutes.js

--- routes/signalementRoutes.js
+++ routes/signalementRoutes.js
@@ -3,15 +3,18 @@ const router = express.Router();
 const multer = require("multer");
 const signalementController = require("../controllers/signalementController");
 
-// Stockage temporaire
-const upload = multer({ dest: "uploads/signalements/" });
+// --- CONFIGURATION MULTER EN MÉMOIRE ---
+// On n'utilise plus { dest: ... } car Render peut bloquer l'écriture
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage });
 
 // --- Routes ---
 router.post(
   "/",
-  upload.single("photo"),
-  signalementController.createSignalement,
+  upload.single("photo"), // Le nom "photo" doit correspondre à ton code Flutter
+  signalementController.createSignalement
 );
+
 router.get("/user/:userId", signalementController.getUserSignalements);
 router.delete("/:id", signalementController.deleteSignalement);
 router.get("/", signalementController.getAllSignalements);
